5beb3d89b7123903e3d7a7cd6d3d8129860466d7
Kevin Tayah <ktayah@yahoo.com>
2023-08-28 11:50:46 -0400

    Revert the naming of envs

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,6 @@
 import Config
 
-base_url = System.get_env("BELAY_API_CLIENT_BASE_URL", "localhost:4000")
+base_url = System.get_env("BELAY_API_BASE_URL", "localhost:4000")
 
 config :belay_api_client,
   env: config_env(),
@@ -9,8 +9,8 @@ config :belay_api_client,
   token_cache_ttl: :timer.minutes(5),
   api_url: (if String.starts_with?(base_url, "localhost"), do: "http://#{base_url}", else: "https://#{base_url}"),
   ws_url: (if String.starts_with?(base_url, "localhost"), do: "ws://#{base_url}", else: "wss://#{base_url}"),
-  partner_id: System.get_env("BELAY_API_CLIENT_PARTNER_ID", "belay_alpaca"),
-  client_id: System.fetch_env!("BELAY_API_CLIENT_PARTNER_CLIENT_ID"),
-  client_secret: System.fetch_env!("BELAY_API_CLIENT_PARTNER_CLIENT_SECRET")
+  partner_id: System.get_env("BELAY_API_PARTNER_ID", "belay_alpaca"),
+  client_id: System.fetch_env!("BELAY_API_AUTH0_CLIENT_ID"),
+  client_secret: System.fetch_env!("BELAY_API_AUTH0_CLIENT_SECRET")
 
 import_config "#{config_env()}.exs"
