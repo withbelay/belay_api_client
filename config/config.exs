d3595bd02b880c3bbee37c4b305084481174b890
Kevin Tayah <ktayah@yahoo.com>
2023-08-28 11:12:30 -0400

    Fix config

--- config/config.exs
+++ config/config.exs
@@ -5,9 +5,9 @@ config :belay_api_client,
   stock_universe: ["AAPL", "TSLA"],
   token_cache_name: :belay_api_cache,
   token_cache_ttl: :timer.minutes(5),
-  api_url: System.get_env("BELAY_API_CLIENT_API_URL") || "http://localhost:4000",
-  offerings_host: System.get_env("BELAY_API_CLIENT_OFFERINGS_HOST") || "ws://localhost:4000",
-  partner_id: System.fetch_env!("BELAY_API_CLIENT_PARTNER_ID") || "belay_alpaca",
+  api_url: System.get_env("BELAY_API_CLIENT_API_URL", "http://localhost:4000"),
+  offerings_host: System.get_env("BELAY_API_CLIENT_OFFERINGS_HOST", "ws://localhost:4000"),
+  partner_id: System.get_env("BELAY_API_CLIENT_PARTNER_ID", "belay_alpaca"),
   client_id: System.fetch_env!("BELAY_API_CLIENT_PARTNER_CLIENT_ID"),
   client_secret: System.fetch_env!("BELAY_API_CLIENT_PARTNER_CLIENT_SECRET")
 
