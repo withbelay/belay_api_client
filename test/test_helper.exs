c357b23124c6e1ea5100688eb439c70d490f2efa
Kevin Tayah <ktayah@yahoo.com>
2023-08-31 14:52:00 -0400

    Fix test_helper

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -6,10 +6,10 @@ is_smoke = :smoke in ExUnit.configuration()[:include]
 
 cond do
   is_smoke and Time.after?(current_time, ~T[14:30:00]) and Time.before?(current_time, ~T[21:00:00]) ->
-    ExUnit.start(include: [smoke_open_hours: true])
+    ExUnit.start(include: [smoke_open_hours: true], exclude: [integration: true, smoke_closed_hours: true])
 
   is_smoke ->
-    ExUnit.start(include: [smoke_closed_hours: true])
+    ExUnit.start(include: [smoke_closed_hours: true], exclude: [integration: true, smoke_open_hours: true])
 
   true ->
     ExUnit.start(exclude: [integration: true, smoke_open_hours: true, smoke_closed_hours: true])
