c1b9dc6ae5fe36897ad136526d5c091dc5125026
Kevin Tayah <ktayah@yahoo.com>
2023-08-31 14:52:15 -0400

    Add offerings off-market test

--- test/smoke/offerings_test.exs
+++ test/smoke/offerings_test.exs
@@ -38,6 +38,18 @@ defmodule Smoke.OfferingsTest do
     @describetag :smoke_closed_hours
 
     test "there are no offerings" do
+      opts = Application.get_all_env(:belay_api_client)
+      client_id = Keyword.fetch!(opts, :client_id)
+      client_secret = Keyword.fetch!(opts, :client_secret)
+      host = Keyword.fetch!(opts, :ws_url)
+
+      {:commit, %{access_token: token}} = BelayApiClient.fetch_token(client_id, client_secret)
+
+      start_supervised!({PartnerSocket, test_pid: self(), host: host, token: token, stock_universe: [@sym]})
+
+      expected_topic = "offerings:#{@sym}"
+
+      assert_receive {^expected_topic, :joined, []}
     end
   end
 end
