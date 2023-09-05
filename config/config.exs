86965138763a122b5200a6916fbfe69ce948e709
surrsurus <surrsurus@protonmail.com>
2023-09-05 11:27:45 -0400

    Add cached token key to config

--- config/config.exs
+++ config/config.exs
@@ -5,5 +5,6 @@ config :belay_api_client,
   partner_id: System.get_env("BELAY_API_PARTNER_ID", "belay_alpaca"),
   client_id: System.fetch_env!("BELAY_API_AUTH0_CLIENT_ID"),
   client_secret: System.fetch_env!("BELAY_API_AUTH0_CLIENT_SECRET")
+  # cached_token: System.fetch_env!("BELAY_API_TOKEN")
 
 import_config "#{config_env()}.exs"
