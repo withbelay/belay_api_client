527407f686a0ccdd1beec71ddf820ee2466221ba
Kevin Tayah <ktayah@yahoo.com>
2023-11-29 14:13:37 -0500

    Adjust smoke tests to new channel behaviour

--- config/config.exs
+++ config/config.exs
@@ -2,7 +2,7 @@ import Config
 
 config :belay_api_client,
   api_url: System.get_env("BELAY_API_URL", "http://localhost:4000"),
-  partner_id: System.get_env("BELAY_API_PARTNER_ID", "belay_alpaca"),
+  partner_id: System.get_env("BELAY_API_PARTNER_ID", "belayalpaca"),
   client_id: System.fetch_env!("BELAYALPACA__AUTH0__CLIENT_ID"),
   client_secret: System.fetch_env!("BELAYALPACA__AUTH0__CLIENT_SECRET")
 
