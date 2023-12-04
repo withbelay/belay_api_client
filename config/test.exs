f9d744b50abcdb47ada48c6481094684a1f8233f
Kevin Tayah <ktayah@yahoo.com>
2023-12-04 11:15:02 -0500

    Pull in alpaca_investors, utilize it to cleanup tests and delete files

--- config/test.exs
+++ config/test.exs
@@ -4,7 +4,7 @@ config :belay_api_client,
   stock_universe: ["AAPL", "TSLA"],
   ws_url: System.get_env("BELAY_API_WS_URL", "ws://localhost:4000")
 
-config :belay_api_client, Alpaca,
+config :alpaca_investors, AlpacaInvestors,
   base_url: System.get_env("ALPACA_BASE_URL", "https://broker-api.sandbox.alpaca.markets"),
   key: System.fetch_env!("BELAYALPACA__ALPACA__KEY"),
   secret: System.fetch_env!("BELAYALPACA__ALPACA__SECRET")
